094073119983e58865015e089088bc8751d48b83
kayke da silva ferreira <kaykedasilvaferreira09@gmail.com>
2025-11-09 18:08:56 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-// Importando os módulos que vamos usar do Firebase (sem o Analytics)
+// Importando os módulos do Firebase (sem Analytics)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
@@ -8,10 +8,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyCkz6E2D2qnUKE5M03ALu_uqCZK7JLQp3Y",
   authDomain: "alldev---plano-b.firebaseapp.com",
   projectId: "alldev---plano-b",
-  storageBucket: "alldev---plano-b.appspot.com",
+  storageBucket: "alldev---plano-b.appspot.com",   // Corrigido
   messagingSenderId: "766087243717",
   appId: "1:766087243717:web:d77ece57140c7d748b8dac",
-  measurementId: "G-3PFEXYL9XC"
+  measurementId: "G-3PFEXYL9XC"                  // Corrigido
 };
 
 // Inicializando o Firebase
